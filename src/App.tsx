386db54e447dfbb525a17b68d212fc507a348f2d
PramisGrg <gurungpramis361@gmail.com>
2024-08-19 19:49:23 +0545

    Initail commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,59 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
+interface TTodo {
+  title: string;
+  description: string;
+}
+
 function App() {
+  const [todo, setTodo] = useState<TTodo>({
+    title: "",
+    description: "",
+  });
+  const [allTodo, setAllTodo] = useState<TTodo[]>([]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setTodo((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleClick = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setAllTodo((prev) => [...prev, todo]);
+  };
   return (
     <>
       <div>
-        <h1>My name is Pramis</h1>
+        <form onSubmit={handleClick}>
+          <label>Enter your title </label>
+          <input
+            placeholder="Enter your title"
+            name="title"
+            onChange={handleChange}
+            value={todo.title}
+          ></input>
+          <label>Enter your description</label>
+          <input
+            placeholder="Enter your description"
+            onChange={handleChange}
+            name="description"
+            value={todo.description}
+          ></input>
+          <button type="submit">Add Todo</button>
+        </form>
+      </div>
+      <div>
+        <div>
+          {allTodo.map((item, index) => (
+            <div key={index}>
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
