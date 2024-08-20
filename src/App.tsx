749b7dea0713cce2712d908b699a8c8b0390fcbb
PramisGrg <gurungpramis361@gmail.com>
2024-08-20 08:29:31 +0545

    fix: Adding router

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,12 @@
-import { ChangeEvent, FormEvent, useState } from "react";
-import TodoCards from "./components/TodoCards";
-
-interface TTodo {
-  title: string;
-  description: string;
-}
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [todo, setTodo] = useState<TTodo>({
-    title: "",
-    description: "",
-  });
-  const [allTodo, setAllTodo] = useState<TTodo[]>([]);
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setTodo((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  const handleClick = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setAllTodo((prev) => [...prev, todo]);
-  };
   return (
     <>
-      <div>
-        <form onSubmit={handleClick}>
-          <label>Enter your title </label>
-          <input
-            placeholder="Enter your title"
-            name="title"
-            onChange={handleChange}
-            value={todo.title}
-          ></input>
-          <label>Enter your description</label>
-          <input
-            placeholder="Enter your description"
-            onChange={handleChange}
-            name="description"
-            value={todo.description}
-          ></input>
-          <button type="submit">Add Todo</button>
-        </form>
-      </div>
-      <div>
-        <div>
-          {allTodo.map((item, index) => (
-            <TodoCards item={item} key={index} />
-          ))}
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <h1>My name is Pramis</h1>
     </>
   );
 }
